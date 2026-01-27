a24eb7c543cc6705b3decaefd375186fb36fba6d
Steve Gontzes <gontzess@gmail.com>
2026-01-27 15:22:26 -0500

    Upgrade baton-sdk to v0.7.4 and update CI configuration
    - Upgrade baton-sdk from v0.2.90 to v0.7.4
    - Upgrade Go to 1.25.2
    - Add WithDefaultCapabilitiesConnectorBuilder for capabilities command
    - Update CI to use go-version-file instead of hardcoded version
    - Update golangci-lint-action to v8 and config to v2 format

--- cmd/baton-pulumi-cloud/main.go
+++ cmd/baton-pulumi-cloud/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/conductorone/baton-pulumi-cloud/pkg/connector"
 	"github.com/conductorone/baton-sdk/pkg/config"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
+	"github.com/conductorone/baton-sdk/pkg/connectorrunner"
 	"github.com/conductorone/baton-sdk/pkg/field"
 	"github.com/conductorone/baton-sdk/pkg/types"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
@@ -28,6 +29,7 @@ func main() {
 		field.Configuration{
 			Fields: ConfigurationFields,
 		},
+		connectorrunner.WithDefaultCapabilitiesConnectorBuilder(&connector.Connector{}),
 	)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
